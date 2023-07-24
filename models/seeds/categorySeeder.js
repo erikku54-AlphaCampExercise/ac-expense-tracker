77fc57aa5b5835a9da476d6ffa75b09511e51bcf
Eric Huang <86519473+erikku54@users.noreply.github.com>
2023-07-24 13:59:46 +0800

    feat: revise user data seeder for using bcrypt
    將seeder也改為加密處理使用者密碼

--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -3,6 +3,8 @@
 //   require('dotenv').config({ path: './.env' });
 // }
 
+const bcrypt = require('bcryptjs');
+
 const db = require('../../config/mongoose');
 
 const Category = require('../categoryModel');
@@ -10,6 +12,11 @@ const User = require('../userModel');
 const { categories, users } = require('./seeds.json');
 
 db.once('open', () => {
+
+  users.forEach(user => {
+    const salt = bcrypt.genSaltSync(10);
+    user.password = bcrypt.hashSync(user.password, salt);
+  })
 
   Promise.all([Category.create(categories), User.create(users)])
     .then(() => {
